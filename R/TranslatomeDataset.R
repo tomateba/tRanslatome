1d91b49fabf2696ab912dc47f96a05d0152d6dba
e.dassi <e.dassi@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-24 12:12:27 +0000

    bugfix for FC computation on condition 2 when data type = "ngs"
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tRanslatome@86718 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TranslatomeDataset.R
+++ R/TranslatomeDataset.R
@@ -156,7 +156,7 @@ setMethod("computeDEGs", "TranslatomeDataset",
 						function(x) mean(x[which(cond.2.vector == 1)],na.rm=TRUE) - 
 												mean(x[which(cond.2.vector == 0)],na.rm=TRUE))
 		if (object@data.type  ==  "ngs") 
-      FC <- apply(cond.2, 1,
+      FC2 <- apply(cond.2, 1,
 						function(x) log(mean(x[which(cond.2.vector == 1)],na.rm=TRUE) / 
 														mean(x[which(cond.2.vector == 0)],na.rm=TRUE),base=2))
     avg.trt2 <- apply(cond.2, 1,
